84bbe50295af5645cea738df92ed57a79c397ef4
Adam <adam.genshaft@globality-contractor.com>
2019-08-17 18:46:26 +0300

    remove comment

--- src/util/translate.ts
+++ src/util/translate.ts
@@ -14,34 +14,6 @@ export const translate = async (word = 'ن‍‍س‍‍ا‍ك') => {
         cache: 'no-cache',
         referrer: 'no-referrer'
     })
-        .then(res => res.json())
-    // .then(res => res.data.translations[0].translatedText);
+        .then(res => res.json());
 
 }
-
-
-// export const translate = async (word = 'ن‍‍س‍‍ا‍ك') => {
-//     if (!word) {
-//         return;
-//     }
-//     const url = new URL('https://translation.googleapis.com/language/translate/v2');
-//     const params = {
-//         key: 'AIzaSyC2BsJQJnYOvdpE37M8DvbLKnlf7iMmAQ8',
-//         target: 'en',
-//         source: 'ar',
-//         q: word
-//     }
-//     type paramKeys = keyof typeof params;
-//     const paramKeys = Object.keys(params) as paramKeys[];
-//     paramKeys.forEach(_key => url.searchParams.append(_key, params[_key]))
-
-//     return fetch(url.toString(), {
-//         method: 'GET',
-//         mode: 'cors',
-//         cache: 'no-cache',
-//         referrer: 'no-referrer'
-//     })
-//         .then(res => res.json())
-//         .then(res => res.data.translations[0].translatedText);
-
-// }
